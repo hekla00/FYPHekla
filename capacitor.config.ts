8b74b0fb09308f2ce953653c350625d2fe3b75d0
Hekla <heklasamuelsdottir00@gmail.com>
2024-01-03 18:23:04 +0000

    Project setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'FYPHekla',
-  webDir: 'dist',
+  appId: "com.fyp.app",
+  appName: "LibraryMate",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { SplashScreen } from "@capacitor/splash-screen";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+SplashScreen.hide();
